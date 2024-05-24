7efb5030537ca41dbcea2a285530b807d4b22291
Heverson Morais <heversondmorais@gmail.com>
2024-05-24 17:54:14 -0300

    projeto base completo

--- src/coleta/coleta/settings.py
+++ src/coleta/coleta/settings.py
@@ -19,7 +19,7 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101
 #USER_AGENT = "coleta (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
