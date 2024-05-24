20f3168b7a6213d7b017939cdf988dd7a36e7b9a
Heverson Morais <heversondmorais@gmail.com>
2024-05-24 19:39:06 -0300

    corrigido local do arquivo em dashboard/app.py

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sqlite3
 
 # Conectar ao banco de dados SQlite
-conn = sqlite3.connect('C:\myprojects\pipeline_etl_python\data\quotes.db')
+conn = sqlite3.connect('..\..\data\quotes.db')
 
 # Carregar os dados da tabela 'mercadolivre_items' em um DataFrame pandas
 df = pd.read_sql_query("SELECT * FROM mercadolivre_items", conn)
